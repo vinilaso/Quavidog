ec54775e0ce87c6176487526b80ea3fdbb9a1590
Vini Laso <vinicius.lanser@gmail.com>
2023-12-28 12:09:52 -0300

    Added Pet.cs

---
+++ Commons/Enums/Breed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commons.Enums
+{
+    public enum Breed
+    {
+        None = 0,
+        Pitbull = 1,
+        GoldenRetriever = 2,
+        ShihTzu = 3,
+        Bulldog = 4,
+        Husky = 5,
+        Poodle = 6,
+        Other = 7
+    }
+}
---
+++ Generics/Entities/Pet.cs
@@ -0,0 +1,62 @@
+﻿using Commons.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.Entities
+{
+    public class Pet
+    {
+        #region Attributes
+
+        public int Id { get; set; }
+        public User Owner { get; set; } = new User();
+        public string Name { get; set; } = string.Empty;
+        public PetSize Size { get; set; }
+        public Breed Breed { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Pet() { }
+
+        #endregion
+
+        #region Methods
+
+        public void Add()
+        {
+
+        }
+
+        public void ReadOne()
+        {
+
+        }
+
+        public void ReadAllByUser()
+        {
+
+        }
+
+        public void ReadAll()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+
+        #endregion
+    }
+}
