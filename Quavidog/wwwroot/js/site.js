f0e2cce17657686291882e66d9faadf2b46c503c
Vini Laso <vinicius.lanser@gmail.com>
2023-12-29 04:00:30 -0300

    Home page added

--- Quavidog/wwwroot/js/site.js
+++ Quavidog/wwwroot/js/site.js
@@ -1,4 +1,39 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//SLIDESHOW
+let slideIndex = 1;
+showSlide(slideIndex);
 
-// Write your JavaScript code.
+// Next/Previous controls
+function plusSlides(n) {
+    showSlide(slideIndex += n);
+    stopLoop();
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+//BANANA DOG
+const bush = document.getElementById("bush");
+const bananaDog = document.getElementById("banana-dog");
+
+bush.addEventListener('mouseenter', () => {
+    bananaDog.style.bottom = '18vh';
+});
+
+bush.addEventListener('mouseleave', () => {
+    bananaDog.style.bottom = '0';
+})
